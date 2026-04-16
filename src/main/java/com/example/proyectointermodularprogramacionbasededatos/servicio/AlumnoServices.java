68196d3793cc2d81ce942f1b2322b2d7948702dd
Alberto412 <anavmol2011@g.educaand.es>
2026-04-16 12:50:37 +0200

    servicios completos 2

--- src/main/java/com/example/proyectointermodularprogramacionbasededatos/servicio/AlumnoServices.java
+++ src/main/java/com/example/proyectointermodularprogramacionbasededatos/servicio/AlumnoServices.java
@@ -4,6 +4,8 @@ import com.example.proyectointermodularprogramacionbasededatos.dominio.Alumno;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 @AllArgsConstructor
 @Service //lo convierte en un servicio
 public class AlumnoServices {
@@ -13,12 +15,12 @@ public class AlumnoServices {
     public Alumno getAlumno(){
         return new Alumno("Alberto","Navarro",18,1);
     }
-    /*
-    @GetMapping({"/saludo/{name}"})
-    public saludar(){
 
+    @GetMapping("/saludo/{name}")
+    public String saludar(@PathVariable("name") String alumno) {
+        return "Hola " + alumno;
     }
 
-     */
+
 
 }
