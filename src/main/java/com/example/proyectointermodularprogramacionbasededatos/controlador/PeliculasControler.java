15d86c6816eb8233c161515d88e82ad477ad2b3a
Alberto412 <anavmol2011@g.educaand.es>
2026-04-16 14:50:29 +0200

    peliculas 3

--- src/main/java/com/example/proyectointermodularprogramacionbasededatos/controlador/PeliculasControler.java
+++ src/main/java/com/example/proyectointermodularprogramacionbasededatos/controlador/PeliculasControler.java
@@ -1,5 +1,6 @@
 package com.example.proyectointermodularprogramacionbasededatos.controlador;
 
+import com.example.proyectointermodularprogramacionbasededatos.dominio.Pelicula;
 import com.example.proyectointermodularprogramacionbasededatos.servicio.AlumnoServices;
 import com.example.proyectointermodularprogramacionbasededatos.servicio.PeliculasService;
 import lombok.AllArgsConstructor;
@@ -7,10 +8,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
 @AllArgsConstructor
 @RestController
 @RequestMapping ("/pelicula")
 public class PeliculasControler {
     private final PeliculasService peliculasService;
+    @GetMapping
+    public ArrayList<Pelicula> mostrar_peliculas(){return peliculasService.mostrar_peliculas();}
+    @GetMapping
+    public Pelicula addPelicula(Pelicula p){return peliculasService.addPelicula(p);}
+
 
 }
