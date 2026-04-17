eeb8abc617d5b4f783311c157c88dc611b73febe
Alberto412 <anavmol2011@g.educaand.es>
2026-04-17 11:10:06 +0200

    base de datos

--- src/main/java/com/example/proyectointermodularprogramacionbasededatos/dominio/Actor.java
+++ src/main/java/com/example/proyectointermodularprogramacionbasededatos/dominio/Actor.java
@@ -1,5 +1,9 @@
 package com.example.proyectointermodularprogramacionbasededatos.dominio;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +11,10 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Entity
 public class Actor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
     private String Nacionalidad;
